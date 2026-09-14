21925d6569fe2cd67ab361fc1e327f346f075404
sagnikc395 <sagnikchatterjee607@gmail.com>
2026-09-14 13:27:13 -0400

    fix: added the embedding dim

--- mini_llm/training/gpt_model.py
+++ mini_llm/training/gpt_model.py
@@ -1,16 +1,36 @@
 import torch
 from torch import nn
 from mini_llm.config import GPT_CONFIG_124M as cfg
-from mini_llm.training.mini_llm_gpt import TransformerBlock
+from mini_llm.training.mini_llm_gpt import TransformerBlock, LayerNorm
 
 
 class GPTModel(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self, cfg=cfg):
         super().__init__()
         self.tok_emb = nn.Embedding(cfg.vocab_size, cfg.emb_dim)
-        self.pos_emb = nn.Embedding(
-            cfg.context_length,
-        )
+        self.pos_emb = nn.Embedding(cfg.context_length, cfg.emb_dim)
         self.drop_emb = nn.Dropout(cfg.drop_rate)
 
-        self.trf_blocks = nn.Sequential(*[])
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg.n_layers)]
+        )
+
+        self.final_norm = LayerNorm(cfg.emb_dim)
+
+        self.out_head = nn.Linear(cfg.emb_dim, cfg.vocab_size, bias=False)
+
+    def forward(self, in_idx):
+        # device setting will allow us to train the model on a CPU or GPU
+        # depending on which device the input data sits on
+        batch_size, seq_len = in_idx.shape
+
+        tok_embeds = self.tok_emb(in_idx)
+        # arange will return a 1D tensor
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
