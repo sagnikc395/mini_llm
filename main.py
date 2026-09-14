6c83379fe38ff2448c61a5598b6b4460046f550b
sagnikc395 <sagnikchatterjee607@gmail.com>
2026-09-14 14:25:49 -0400

    feat: shape is same

--- main.py
+++ main.py
@@ -21,10 +21,18 @@ def main():
     # 2. initialize instance and feed it the tokenized batch
     torch.manual_seed(123)
     model = GPTModel(GPT_CONFIG_124M)
+    total_params = sum(p.numel() for p in model.parameters())
     logits = model(batch)
     print(f"Input batch: \n{batch}")
     print(f"Output shape: \n{logits.shape}")
+    print(
+        f"Total number of parameters: {total_params}\n, is it 124 million ? {True if (124_000_000 == total_params) else False}"
+    )
     print(logits)
+
+    # print the outputs , the weight tensors for both of these layers and checking if they have the same shape or not
+    print(f"token embedding layer shape: {model.tok_emb.weight.shape}")
+    print(f"output layer shape: {model.out_head.weight.shape}")
 
 
 if __name__ == "__main__":
